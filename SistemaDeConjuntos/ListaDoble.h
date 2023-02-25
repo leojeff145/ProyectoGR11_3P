05c21064d1113acf4e00f1eea12a9992e4cd19f6
Leonardo <jlobando2@espe.edu.ec>
2023-02-25 12:33:26 -0500

    lista

--- SistemaDeConjuntos/ListaDoble.h
+++ SistemaDeConjuntos/ListaDoble.h
@@ -9,8 +9,8 @@ private:
 	NodoDoble<T>* cola;
 	int tamanio;
 public:
-	Lista_Doble();
-	~Lista_Doble();
+	ListaDoble();
+	~ListaDoble();
 	bool estaVacia();
 	int getTamanio();
 	void insertarInicio(T dato);
@@ -21,3 +21,179 @@ public:
 	void eliminarPosicion(int posicion);
 	void mostrar();
 };
+
+template <typename T>
+ListaDoble<T>::ListaDoble()
+{
+	this->cabeza = nullptr;
+	this->cola = nullptr;
+	this->tamanio = 0;
+}
+
+template <typename T>
+ListaDoble<T>::~ListaDoble()
+{
+}
+
+template <typename T>
+bool ListaDoble<T>::estaVacia()
+{
+	return this->cabeza == nullptr;
+}
+
+template <typename T>
+int ListaDoble<T>::getTamanio()
+{
+	return this->tamanio;
+}
+
+template <typename T>
+void ListaDoble<T>::insertarInicio(T dato)
+{
+	NodoDoble<T>* nuevo = new NodoDoble<T>(dato);
+	if (estaVacia())
+	{
+		this->cola = nuevo;
+	}
+	else
+	{
+		this->cabeza->setAnterior(nuevo);
+		nuevo->setSiguiente(this->cabeza);
+	}
+	this->cabeza = nuevo;
+	this->tamanio++;
+}
+
+template <typename T>
+void ListaDoble<T>::insertarFinal(T dato)
+{
+	NodoDoble<T>* nuevo = new NodoDoble<T>(dato);
+	if (estaVacia())
+	{
+		this->cabeza = nuevo;
+	}
+	else
+	{
+		this->cola->setSiguiente(nuevo);
+		nuevo->setAnterior(this->cola);
+	}
+	this->cola = nuevo;
+	this->tamanio++;
+}
+
+template <typename T>
+void ListaDoble<T>::insertarPosicion(T dato, int posicion)
+{
+	if (posicion < 0 || posicion > this->tamanio)
+	{
+		throw "Posicion no valida";
+	}
+	else if (posicion == 0)
+	{
+		insertarInicio(dato);
+	}
+	else if (posicion == this->tamanio)
+	{
+		insertarFinal(dato);
+	}
+	else
+	{
+		NodoDoble<T>* nuevo = new NodoDoble<T>(dato);
+		NodoDoble<T>* aux = this->cabeza;
+		for (int i = 0; i < posicion; i++)
+		{
+			aux = aux->getSiguiente();
+		}
+		nuevo->setSiguiente(aux);
+		nuevo->setAnterior(aux->getAnterior());
+		aux->getAnterior()->setSiguiente(nuevo);
+		aux->setAnterior(nuevo);
+		this->tamanio++;
+	}
+}
+
+template <typename T>
+void ListaDoble<T>::eliminarInicio()
+{
+	if (estaVacia())
+	{
+		throw "Lista vacia";
+	}
+	else if (this->cabeza == this->cola)
+	{
+		this->cabeza = nullptr;
+		this->cola = nullptr;
+	}
+	else
+	{
+		this->cabeza = this->cabeza->getSiguiente();
+		this->cabeza->setAnterior(nullptr);
+	}
+	this->tamanio--;
+}
+
+template <typename T>
+void ListaDoble<T>::eliminarFinal()
+{
+	if (estaVacia())
+	{
+		throw "Lista vacia";
+	}
+	else if (this->cabeza == this->cola)
+	{
+		this->cabeza = nullptr;
+		this->cola = nullptr;
+	}
+	else
+	{
+		this->cola = this->cola->getAnterior();
+		this->cola->setSiguiente(nullptr);
+	}
+	this->tamanio--;
+}
+
+template <typename T>
+void ListaDoble<T>::eliminarPosicion(int posicion)
+{
+	if (estaVacia())
+	{
+		throw "Lista vacia";
+	}
+	else if (posicion < 0 || posicion >= this->tamanio)
+	{
+		throw "Posicion no valida";
+	}
+	else if (posicion == 0)
+	{
+		eliminarInicio();
+	}
+	else if (posicion == this->tamanio - 1)
+	{
+		eliminarFinal();
+	}
+	else
+	{
+		NodoDoble<T>* aux = this->cabeza;
+		for (int i = 0; i < posicion; i++)
+		{
+			aux = aux->getSiguiente();
+		}
+		aux->getAnterior()->setSiguiente(aux->getSiguiente());
+		aux->getSiguiente()->setAnterior(aux->getAnterior());
+		this->tamanio--;
+	}
+}
+
+template <typename T>
+void ListaDoble<T>::mostrar()
+{
+	NodoDoble<T>* aux = this->cabeza;
+	while (aux != nullptr)
+	{
+		cout << aux->getDato() << " ";
+		aux = aux->getSiguiente();
+	}
+	cout << endl;
+}
+
+
