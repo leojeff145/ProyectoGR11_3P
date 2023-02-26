8e0e47bceac7d72264e187c02099c97c1950ce1c
Leonardo <jlobando2@espe.edu.ec>
2023-02-26 13:09:02 -0500

    diagrama

--- SistemaDeConjuntos/DiagramaVenn.h
+++ SistemaDeConjuntos/DiagramaVenn.h
@@ -1,5 +1,21 @@
 #pragma once
+#include "Conjunto.h"
+#include <vector>
 class DiagramaVenn
 {
+private:
+	Conjunto _a;
+	Conjunto _b;
+	Conjunto _izquierda;
+	Conjunto _derecha;
+	Conjunto _interseccion;
+
+	void calcular();
+public: 
+	DiagramaVenn(Conjunto a, Conjunto b);
+
+	std::vector<int> getIzquierda();
+	std::vector<int> getDerecha();
+	std::vector<int> getInterseccion();
 };
 
