76b4f5452d7863f0f8925061325c742c21d25d4b
Leonardo <jlobando2@espe.edu.ec>
2023-02-27 07:03:41 -0500

    test

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -1,16 +1,19 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "../OperacionConjuntos/GraphSet.h"
+
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTest1
 {
-	TEST_CLASS(UnitTest1)
+	TEST_CLASS(GraphSetTest)
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
-		{
+		TEST_METHOD(drawBigO_nlogn())
+		{GraphSet graph;
+			graph.drawBigO_nlogn();
 		}
 	};
 }
